258f1cca3ef9e51cbbe1a8db114beef5a903133b
Replit user <>
2023-07-27 00:04:24 +0000

    initial

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('/hello'):
+        await message.channel.send('Hello!')
+    
+
+client.run(os.getenv('TOKEN'))
